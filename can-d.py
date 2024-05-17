7b274f34bb71e5f431df9c94fe15746b3b965a9e
Marko Morrison <markomoic@gmail.com>
2024-05-17 16:42:35 -0700

    Now outputs files to csv_storage

--- can-d.py
+++ can-d.py
@@ -1,9 +1,11 @@
 import random
+import datetime
 
 numColumns = 5
 numRows = 50
 tellingCredLoc = int((random.random() * ((numRows - 1) / 2)) + (numRows / 2))#random point past halfway 
-print("Your telling index is:",tellingCredLoc)
+print("Your telling credential is at entry",tellingCredLoc)
+print("-------------------------------------")
 
 def generateData():
     #Set up table
@@ -32,17 +34,36 @@ def generateData():
             specialUser = random.choice(usernameChoices) + random.choice(usernameChoices) + random.choice(usernameChoices)
             specialPassword = random.choice(passwordChoices) + random.choice(passwordChoices) + random.choice(passwordChoices)
             output[x] = [x, random.choice(firstNameChoices), random.choice(lastNameChoices), specialUser, specialPassword]
-
+            print("Your telling credentials are:")
+            print("UN:",specialUser)
+            print("PW:",specialPassword)
+            print("RECORD THESE NOW!")
+            print("-------------------------------------")
     return output
 
 def printTableAsCSV(input):
     for x in range(0, numRows): #For each row:
-    	for y in range(0, numColumns - 1): #For each entry, print in csv format
-    		print(input[x][y],",",end='',sep='')
+        for y in range(0, numColumns - 1): #For each entry, print in csv format
+            print(input[x][y],",",end='',sep='')
         print(input[x][numColumns - 1])
 
+def outputTableToCSVFile(input):
+    timestamp = datetime.datetime.now()
+    filename = str(timestamp) + ".csv"
+    filepath = "./csv_storage/" + filename
+    outputfile = open(filepath, "w+")
+    for x in range(0, numRows): #For each row:
+        for y in range(0, numColumns - 1): #For each entry, print in csv format
+            print(input[x][y],",",end='',sep='',file=outputfile)
+        print(input[x][numColumns - 1],file=outputfile)
+    print("Printing to file with timestamp:",timestamp,"in ./csv_storage")
+    
+
 #MAIN
+
+#Create table
 myAdmin = generateData()
-printTableAsCSV(myAdmin)
+#Print it as CSV
+outputTableToCSVFile(myAdmin)
 
 
