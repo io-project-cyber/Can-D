a66b44d90c2a967778a41bdc8de85d2a7b6a209c
Marko Morrison <88122773+ouro-borous@users.noreply.github.com>
2024-05-29 21:30:58 -0700

    Update install-can-d.sh

--- helper-scripts/install-can-d.sh
+++ helper-scripts/install-can-d.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/ouro-borous/Can-D.git
 cd ./Can-D/
 python3 -m venv venv
-source /venv/bin/activate
+source venv/bin/activate
 pip install names-dataset
 echo "~~~~~~~"
 echo "!READY!"
