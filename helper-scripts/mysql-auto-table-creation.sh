6f2d5a47075243335188254b399fe911d834e89a
Marko Morrison <markomoic@gmail.com>
2024-05-29 21:49:19 -0700

    Fixed MySQL helper script

--- helper-scripts/mysql-auto-table-creation.sh
+++ helper-scripts/mysql-auto-table-creation.sh
@@ -10,11 +10,9 @@ do
 done
 
 cd ..
-
-if [ ! -f ./csv-storage/toImport.csv ];
-    #If csv-storage/toImposrt.csv doesn't exist, THIS COMMAND IS RAN.
-    ./can-d.py -v -pO -o ./csv-storage/toImport.csv
-fi
+                                     #If csv-storage/toImport.csv doesn't exist, THIS COMMAND IS RAN.
+[ -f ./csv-storage/toImport.csv ] || ./can-d.py -v -pO -o ./csv-storage/toImport.csv
+    
 toImportFilePath="'"
 toImportFilePath+=$(pwd)
 toImportFilePath+="/csv-storage/toImport.csv'"
@@ -24,5 +22,9 @@ echo "SET GLOBAL local_infile=1;" >> sql-server-commands.txt
 sudo mysql < sql-server-commands.txt
 
 echo "USE $database_name;" > sql-server-commands.txt
-echo "CREATE TABLE $table_name (ID INT AUTO_INCREMENT PRIMARY KEY, firstName VARCHAR(100), lastName VARCHAR(100), username VARCHAR(250), password VARCHAR(200));" > sql-server-commands.txt
-echo "LOAD DATA LOCAL INFILE $toImportFilePath INTO TABLE $table_name FIELDS TERMINATED BY ',' IGNORE 1 ROWS;" > sql-server-commands.txt
+echo "CREATE TABLE $table_name (ID INT AUTO_INCREMENT PRIMARY KEY, firstName VARCHAR(100), lastName VARCHAR(100), username VARCHAR(250), password VARCHAR(200));" >> sql-server-commands.txt
+echo "LOAD DATA LOCAL INFILE $toImportFilePath INTO TABLE $table_name FIELDS TERMINATED BY ',' IGNORE 1 ROWS;" >> sql-server-commands.txt
+sudo mysql < sql-server-commands.txt
+
+echo "SET GLOBAL local_infile=0;" > sql-server-commands.txt
+sudo mysql < sql-server-commands.txt
